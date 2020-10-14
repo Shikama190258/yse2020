1f9255266350b8c9a8a56905d8737261177650e2
fuku-yse <190263ms@yse-c.net>
2020-10-14 12:02:30 +0900

    Update nyuka.php
    (1),(2),(3),(4),(5),(8),(9),(10),(13),(14),(15)
    　実装済

--- nyuka.php
+++ nyuka.php
@@ -1,5 +1,5 @@
 <?php
-/* 
+/*
 【機能】
 書籍の入荷数を指定する。確定ボタンを押すことで確認画面へ入荷個数を引き継いで遷移す
 る。なお、在庫数は各書籍100冊を最大在庫数とする。
@@ -11,118 +11,133 @@
 */
 
 /*
- * ①session_status()の結果が「PHP_SESSION_NONE」と一致するか判定する。
+ * (1)session_status()の結果が「PHP_SESSION_NONE」と一致するか判定する。
  * 一致した場合はif文の中に入る。
  */
-if (/* ①.の処理を行う */) {
-	//②セッションを開始する
+if (session_status() == PHP_SESSION_NONE) {
+	//(2)セッションを開始する
+	session_start();
 }
 
+//
+$_SESSION['login'] = True; //デバッグ用
+//
 
-//③SESSIONの「login」フラグがfalseか判定する。「login」フラグがfalseの場合はif文の中に入る。
-if (/* ③の処理を書く */){
-	//④SESSIONの「error2」に「ログインしてください」と設定する。
-	//⑤ログイン画面へ遷移する。
+//(3)SESSIONの「login」フラグがfalseか判定する。「login」フラグがfalseの場合はif文の中に入る。
+if ($_SESSION['login'] == False){
+    //(4)SESSIONの「error2」に「ログインしてください」と設定する。
+    $_SESSION['error2'] = "ログインしてください";
+    //(5)ログイン画面へ遷移する。
+    header('Location: login.php');
 }
 
-//⑥データベースへ接続し、接続情報を変数に保存する
+//(6)データベースへ接続し、接続情報を変数に保存する
+//(7)データベースで使用する文字コードを「UTF8」にする
 
-//⑦データベースで使用する文字コードを「UTF8」にする
 
-//⑧POSTの「books」の値が空か判定する。空の場合はif文の中に入る。
-if(/* ⑧の処理を行う */){
-	//⑨SESSIONの「success」に「入荷する商品が選択されていません」と設定する。
-	//⑩在庫一覧画面へ遷移する。
+//(8)POSTの「books」の値が空か判定する。空の場合はif文の中に入る。
+if(! $_POST['books']){
+    //(9)SESSIONの「success」に「入荷する商品が選択されていません」と設定する。
+    $_SESSION['success'] = "入荷する商品が選択されていません";
+    //(10)在庫一覧画面へ遷移する。
+    header('Location: zaiko_ichiran.php');
+
 }
+
+echo $_SESSION['success'];
 
 function getId($id,$con){
-	/* 
-	 * ⑪書籍を取得するSQLを作成する実行する。
+	/*
+	 * (11)書籍を取得するSQLを作成する実行する。
 	 * その際にWHERE句でメソッドの引数の$idに一致する書籍のみ取得する。
 	 * SQLの実行結果を変数に保存する。
 	 */
 
-	//⑫実行した結果から1レコード取得し、returnで値を返す。
+	//(12)実行した結果から1レコード取得し、returnで値を返す。
 }
 
 ?>
 <!DOCTYPE html>
 <html lang="ja">
+
 <head>
-	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-	<title>入荷</title>
-	<link rel="stylesheet" href="css/ichiran.css" type="text/css" />
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <title>入荷</title>
+    <link rel="stylesheet" href="css/ichiran.css" type="text/css" />
 </head>
+
 <body>
-	<!-- ヘッダ -->
-	<div id="header">
-		<h1>入荷</h1>
-	</div>
+    <!-- ヘッダ -->
+    <div id="header">
+        <h1>入荷</h1>
+    </div>
 
-	<!-- メニュー -->
-	<div id="menu">
-		<nav>
-			<ul>
-				<li><a href="zaiko_ichiran.php?page=1">書籍一覧</a></li>
-			</ul>
-		</nav>
-	</div>
+    <!-- メニュー -->
+    <div id="menu">
+        <nav>
+            <ul>
+                <li><a href="zaiko_ichiran.php?page=1">書籍一覧</a></li>
+            </ul>
+        </nav>
+    </div>
 
-	<form action="nyuka_kakunin.php" method="post">
-		<div id="pagebody">
-			<!-- エラーメッセージ -->
-			<div id="error">
-			<?php
+    <form action="nyuka_kakunin.php" method="post">
+        <div id="pagebody">
+            <!-- エラーメッセージ -->
+            <div id="error">
+                <?php
 			/*
-			 * ⑬SESSIONの「error」にメッセージが設定されているかを判定する。
+			 * (13)SESSIONの「error」にメッセージが設定されているかを判定する。
 			 * 設定されていた場合はif文の中に入る。
-			 */ 
-			if(/* ⑬の処理を書く */){
-				//⑭SESSIONの「error」の中身を表示する。
+			 */
+			if($_SESSION['error']){
+                //(14)SESSIONの「error」の中身を表示する。
+                echo $_SESSION['error'];
 			}
 			?>
-			</div>
-			<div id="center">
-				<table>
-					<thead>
-						<tr>
-							<th id="id">ID</th>
-							<th id="book_name">書籍名</th>
-							<th id="author">著者名</th>
-							<th id="salesDate">発売日</th>
-							<th id="itemPrice">金額(円)</th>
-							<th id="stock">在庫数</th>
-							<th id="in">入荷数</th>
-						</tr>
-					</thead>
-					<?php 
+            </div>
+            <div id="center">
+                <table>
+                    <thead>
+                        <tr>
+                            <th id="id">ID</th>
+                            <th id="book_name">書籍名</th>
+                            <th id="author">著者名</th>
+                            <th id="salesDate">発売日</th>
+                            <th id="itemPrice">金額(円)</th>
+                            <th id="stock">在庫数</th>
+                            <th id="in">入荷数</th>
+                        </tr>
+                    </thead>
+                    <?php
 					/*
-					 * ⑮POSTの「books」から一つずつ値を取り出し、変数に保存する。
+					 * (15)POSTの「books」から一つずつ値を取り出し、変数に保存する。
 					 */
-    				foreach(/* ⑮の処理を書く */){
-    					// ⑯「getId」関数を呼び出し、変数に戻り値を入れる。その際引数に⑮の処理で取得した値と⑥のDBの接続情報を渡す。
+    				foreach($_POST['books'] as $book){
+    					// (16)「getId」関数を呼び出し、変数に戻り値を入れる。その際引数に(15)の処理で取得した値と(6)のDBの接続情報を渡す。
 					?>
-					<input type="hidden" value="<?php echo	/* ⑰ ⑯の戻り値からidを取り出し、設定する */;?>" name="books[]">
-					<tr>
-						<td><?php echo	/* ⑱ ⑯の戻り値からidを取り出し、表示する */;?></td>
-						<td><?php echo	/* ⑲ ⑯の戻り値からtitleを取り出し、表示する */;?></td>
-						<td><?php echo	/* ⑳ ⑯の戻り値からauthorを取り出し、表示する */;?></td>
-						<td><?php echo	/* ㉑ ⑯の戻り値からsalesDateを取り出し、表示する */;?></td>
-						<td><?php echo	/* ㉒ ⑯の戻り値からpriceを取り出し、表示する */;?></td>
-						<td><?php echo	/* ㉓ ⑯の戻り値からstockを取り出し、表示する */;?></td>
-						<td><input type='text' name='stock[]' size='5' maxlength='11' required></td>
-					</tr>
-					<?php
-					 }
+                    <input type="hidden" value="<?php echo $book['id'];?>" name="books[]">
+                    <tr>
+                        <td><?php echo "none";?></td> <!-- (18) (16)の戻り値からidを取り出し、表示する-->
+                        <td><?php echo "none";?></td> <!-- /* (19) (16)の戻り値からtitleを取り出し、表示する */ -->
+                        <td><?php echo "none";?></td> <!-- /* (20) (16)の戻り値からauthorを取り出し、表示する */ -->
+                        <td><?php echo "none";?></td> <!-- /* (21) (16)の戻り値からsalesDateを取り出し、表示する */ -->
+                        <td><?php echo "none";?></td> <!-- /* (22) (16)の戻り値からpriceを取り出し、表示する */ -->
+                        <td><?php echo "none";?></td> <!-- /* (23) (16)の戻り値からstockを取り出し、表示する */ -->
+                        <td><input type='text' name='stock[]' size='5' maxlength='11' required></td>
+                    </tr>
+                    <?php
+					}
 					?>
-				</table>
-				<button type="submit" id="kakutei" formmethod="POST" name="decision" value="1">確定</button>
-			</div>
-		</div>
-	</form>
-	<!-- フッター -->
-	<div id="footer">
-		<footer>株式会社アクロイト</footer>
-	</div>
+                </table>
+                <button type="submit" id="kakutei" formmethod="POST" name="decision" value="1">確定</button>
+            </div>
+        </div>
+    </form>
+    <!-- フッター -->
+    <div id="footer">
+        <footer>株式会社アクロイト</footer>
+    </div>
 </body>
+
 </html>
